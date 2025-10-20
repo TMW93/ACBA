0c52474f0f4aede2a117ced12308525542ea0a30
TimWong <timwong.tw93@gmail.com>
2025-10-20 17:34:56 +1100

    added client side utils

---
+++ client/utils/queries.js
@@ -0,0 +1,34 @@
+import {gql} from '@apollo/client';
+
+export const QUERY_SINGLE_USER = gql`
+  query singleUser($userId:ID!) {
+    user(userId: $userId) {
+      _id
+      firstName
+      lastName
+      email
+      teams
+    }
+  }
+`;
+
+export const QUERY_ALL_DIVISIONS = gql`
+  query getDivisions {
+    allDivisions {
+      _id
+      name
+      day
+    }
+  }
+`;
+
+export const QUERY_SINGLE_DIVISION = gql`
+  query getSingleDivision($divisionId: ID!) {
+    division(divisionId: $divisionId) {
+      _id
+      name
+      day
+    }
+  }
+`;
+
