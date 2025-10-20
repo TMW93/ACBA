3ac460e551411dec23501f743ee344d016396227
TimWong <timwong.tw93@gmail.com>
2025-10-20 13:56:30 +1100

    added client main.jsx

---
+++ client/src/main.jsx
@@ -0,0 +1,26 @@
+import ReactDOM from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import './index.css'
+
+import App from './App.jsx'
+import Error from './pages/Error.jsx'
+import Home from './pages/Home.jsx'
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+    ]
+  }
+]);
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router = {router} />
+);
