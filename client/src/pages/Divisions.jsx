5cdf8766b10638817b47def39ab9ed5d6feb2643
TimWong <timwong.tw93@gmail.com>
2025-10-21 16:16:25 +1100

    sorted divs

--- client/src/pages/Divisions.jsx
+++ client/src/pages/Divisions.jsx
@@ -29,23 +29,93 @@ const Divisions = () => {
   //Thursday Divs
   let thursdayDivs = divs.filter(div => div.day.includes("Thursday"));
 
-  console.log(sundayDivs, mondayDivs, tuesdayDivs, wednesdayDivs, thursdayDivs);
+  // console.log(sundayDivs, mondayDivs, tuesdayDivs, wednesdayDivs, thursdayDivs);
 
   return (
     <div>
       <Nav />
       <h1>Divisions</h1>
-      <div>
-        {divs.map((div) => (
-          <div key={div._id} className="max-w-sm rounded overflow-hidden shadow-lg">
-            <img className="w-full" src={ACBALogo} alt="acba logo"/>
-            <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">{div.day}</div>
-              <div className="font-bold text-xl mb-2">{div.name}</div>
-            </div>
+      <div className="flex mb-4">
+        {/* Sunday Divs */}
+        <div className="w=1/5 bg-gray-500 h-12">
+          <h2>Sunday</h2>
+          <h2>Divisions</h2>
+          <div> 
+            {sundayDivs.map((div) => (
+              <div key={div._id} className="max-w-sm rounded overflow-hidden shadow-lg">
+                <img className="w-full" src={ACBALogo} alt="acba logo"/>
+                <div className="px-6 py-4">
+                  <div className="font-bold text-xl mb-2">{div.day}</div>
+                  <div className="font-bold text-xl mb-2">{div.name}</div>
+                </div>
+              </div>
+            ))}
           </div>
-        ))
-        }
+        </div>
+        {/* Monday Divs */}
+        <div className="w=1/5 bg-gray-500 h-12">
+          <h2>Monday</h2>
+          <h2>Divisions</h2>
+          <div>
+            {mondayDivs.map((div) => (
+              <div key={div._id} className="max-w-sm rounded overflow-hidden shadow-lg">
+                <img className="w-full" src={ACBALogo} alt="acba logo"/>
+                <div className="px-6 py-4">
+                  <div className="font-bold text-xl mb-2">{div.day}</div>
+                  <div className="font-bold text-xl mb-2">{div.name}</div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        {/* Tuesday Divs */}
+        <div className="w=1/5 bg-gray-500 h-12">
+          <h2>Tuesday</h2>
+          <h2>Divisions</h2>
+          <div>
+            {tuesdayDivs.map((div) => (
+              <div key={div._id} className="max-w-sm rounded overflow-hidden shadow-lg">
+                <img className="w-full" src={ACBALogo} alt="acba logo"/>
+                <div className="px-6 py-4">
+                  <div className="font-bold text-xl mb-2">{div.day}</div>
+                  <div className="font-bold text-xl mb-2">{div.name}</div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        {/* Wednesday Divs */}
+        <div className="w=1/5 bg-gray-500 h-12">
+          <h2>Wednesday</h2>
+          <h2>Divisions</h2>
+          <div>
+            {wednesdayDivs.map((div) => (
+              <div key={div._id} className="max-w-sm rounded overflow-hidden shadow-lg">
+                <img className="w-full" src={ACBALogo} alt="acba logo"/>
+                <div className="px-6 py-4">
+                  <div className="font-bold text-xl mb-2">{div.day}</div>
+                  <div className="font-bold text-xl mb-2">{div.name}</div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+        {/* Thursday Divs */}
+        <div className="w=1/5 bg-gray-500 h-12">
+          <h2>Thursday</h2>
+          <h2>Divisions</h2>
+          <div>
+          {thursdayDivs.map((div) => (
+            <div key={div._id} className="max-w-sm rounded overflow-hidden shadow-lg">
+              <img className="w-full" src={ACBALogo} alt="acba logo"/>
+              <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">{div.day}</div>
+                <div className="font-bold text-xl mb-2">{div.name}</div>
+              </div>
+            </div>
+          ))}
+          </div>
+        </div>  
       </div>
     </div>
   )
