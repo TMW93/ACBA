fbb119e73b6c4a4cd947ea3f293e0fc38048fb9a
TimWong <timwong.tw93@gmail.com>
2025-10-22 11:12:09 +1100

    updated seeders

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,12 +1,31 @@
 const db = require('../config/connection');
-const {Division} = require('../models');
-const divisionSeeds = require('./divisionSeeds.json');
 const cleanDB = require('./cleanDB');
+const {Division, Team} = require('../models');
+
+const divisionSeeds = require('./divisionSeeds.json');
+const teamSeeds = require('./teamSeeds.json');
 
 db.once('open', async () => {
   await cleanDB('Division', 'divisions');
+  await cleanDB('Team', 'teams');
 
-  await Division.create(divisionSeeds);
+  const divisions = await Division.create(divisionSeeds);
+  const teams = await Team.create(teamSeeds);
+  
+  let i = 0;
+
+  for(const newDivision of divisions) {
+    // seeding 4 teams into each division
+    for(let count = 0; count < 4; count++) {
+      i++;
+      newDivision.teams.push(teams[i]._id);
+      teams[i].division = newDivision._id;
+      await teams[i].save();
+      // console.log(teams[i]);
+    };
+    await newDivision.save();
+    // console.log(newDivision);
+  }
 
   console.log('Seeding Completed.');
 
