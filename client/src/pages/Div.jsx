7e70c300558c76116444bbf5f614910e6ad15314
TimWong <timwong.tw93@gmail.com>
2025-10-22 15:28:20 +1100

    updated div page

--- client/src/pages/Div.jsx
+++ client/src/pages/Div.jsx
@@ -1,6 +1,7 @@
 import {useParams} from 'react-router-dom';
 import {useQuery} from '@apollo/client/react';
 import {QUERY_SINGLE_DIVISION} from '../../utils/queries';
+import {useState} from 'react';
 
 import Nav from "../components/Nav";
 
@@ -26,8 +27,41 @@ const Div = () => {
   return (
     <div>
       <Nav />
-      <h2>{division.day}</h2>
-      <h2>{division.name}</h2>
+      <div>
+        <h2>{division.day}</h2>
+        <h2>{division.name}</h2>
+        <div className='mt-10 grid grid-cols-1 gap-4 sm:mt-16 lg:grid-cols-6 lg:grid-rows-2'>
+          <div className='flex p-px lg:col-span-4'>
+            {/* Table */}
+            <table className="table-auto border-separate border-spacing-2 border border-gray-400 dark:border-gray-500">  
+              <thead>    
+                <tr>      
+                  <th>Position</th>      
+                  <th>Team</th>      
+                  <th>W</th>
+                  <th>L</th>
+                  <th>D</th>
+                  <th>Games Played</th>
+                  <th>TP</th>
+                  <th>Points Behind</th>    
+                </tr>  
+              </thead>  
+              <tbody>
+                {division.teams.map((team, index) => (
+                  <tr key={team._id}>
+                    <td>{index + 1}</td>
+                    <td>{team.name}</td>
+                  </tr>
+                ))}              
+              </tbody>
+            </table>
+          </div>
+          <div className='flex p-px lg:col-span-2'>
+            <p>Upcoming Games</p>
+          </div>
+        </div>
+
+      </div>
     </div>
   )
 }
