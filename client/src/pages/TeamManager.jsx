c16ce20279bc0fa3d4d40812a0d83044209c2a91
TimWong <timwong.tw93@gmail.com>
2025-12-18 22:48:46 +1100

    updated admin page

--- client/src/pages/TeamManager.jsx
+++ client/src/pages/TeamManager.jsx
@@ -7,150 +7,133 @@ import GameScheduler from '../components/GameScheduler'
 import RemoveGamesForm from '../components/RemoveGamesForm'
 import ArchiveGamesForm from '../components/ArchiveGamesForm'
 import ResetSeasonForm from '../components/ResetSeasonForm'
+import { Dialog, DialogBackdrop, DialogPanel, DialogTitle } from '@headlessui/react'
+import { ArchiveBoxIcon, ArrowPathIcon, CalendarIcon, CheckIcon, TrashIcon, UserGroupIcon, UserPlusIcon} from '@heroicons/react/24/outline'
 
 import {useState} from 'react'
 
+const tasks = [
+  {
+    title: 'Add Team',
+    icon: UserPlusIcon,
+    background: 'bg-pink-500',
+    value: 'addTeam',
+  },
+  {
+    title: 'Remove Team',
+    icon: TrashIcon,
+    background: 'bg-red-500',
+    value: 'removeTeam',
+  },
+  {
+    title: 'Add Division',
+    icon: UserGroupIcon,
+    background: 'bg-green-500',
+    value: 'addDivision',
+  },
+  {
+    title: 'Remove Division',
+    icon: TrashIcon,
+    background: 'bg-red-500',
+    value: 'removeDivision',
+  },
+  {
+    title: 'Schedule Games',
+    icon: CalendarIcon,
+    background: 'bg-indigo-500',
+    value: 'gameScheduler',
+  },
+  {
+    title: 'Remove Game/s',
+    icon: TrashIcon,
+    background: 'bg-red-500',
+    value: 'removeGame',
+    
+  },
+  {
+    title: 'Archive Game/s',
+    icon: ArchiveBoxIcon,
+    background: 'bg-sky-500',
+    value: 'archiveGames',
+  },
+  {
+    title: 'Reset Season',
+    icon: ArrowPathIcon,
+    background: 'bg-purple-500',
+    value: 'seasonReset',
+  },
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ');
+};
+
 export default function TeamManager() {
-  const [currentForm, setFormState] = useState('addTeam');
+  const [currentForm, setFormState] = useState('');
+  const [open, setOpen] = useState(false);
   //console.log(currentForm);
+  
+  const handleButtonClick = async (e) => {
+    setOpen(true);
+    setFormState(e.target.id);
+    // console.log(e.target.id);
+    // console.log('im clicked');
+  }
 
   return (
-    <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8 dark:bg-gray-900">
-      <div
-        aria-hidden="true"
-        className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+    <div>
+      <Nav/>
+      <h2 className="text-base mt-5 font-semibold text-gray-900 dark:text-white">Admin Tasks</h2>
+      {/* Task List */}
+      <ul
+        role="list"
+        className="mt-6 grid grid-cols-1 gap-6 border-y border-gray-200 py-6 sm:grid-cols-2 dark:border-white/10"
       >
-        <div
-          style={{
-            clipPath:
-              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-          }}
-          className="relative left-1/2 -z-10 aspect-1155/678 w-144.5 max-w-none -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-288.75 dark:opacity-20"
-        />
-      </div>
-      <Nav />
-      <div className="mx-auto max-w-2xl text-center">
-        <h2 className="text-4xl font-semibold tracking-tight text-balance text-gray-900 sm:text-5xl dark:text-white">
-          Team Manager
-        </h2>
-        {/* mode switcher */}
-        <div className="mt-16 flex justify-center">
-          <fieldset aria-label="Admin">
-            <div className="grid grid-cols-2 gap-x-1 rounded-md p-1 text-center text-xs/5 font-semibold inset-ring inset-ring-gray-200 dark:inset-ring-white/10">
-              {/* Add Team */}
-              <label className="group relative rounded-full px-2.5 py-1 has-checked:bg-indigo-600 dark:has-checked:bg-indigo-500">
-                <input
-                  defaultValue="addTeam"
-                  defaultChecked
-                  name="switcher"
-                  type="radio"
-                  className="absolute inset-0 appearance-none rounded-full"
-                  onClick={() => setFormState('addTeam')}
-                />
-                <span className="text-gray-500 group-has-checked:text-white dark:text-gray-400">Add Team</span>
-              </label>
-              {/* Modify Team */}
-              <label className="group relative rounded-full px-2.5 py-1 has-checked:bg-indigo-600 dark:has-checked:bg-indigo-500">
-                <input
-                  defaultValue="modifyTeam"
-                  name="switcher"
-                  type="radio"
-                  className="absolute inset-0 appearance-none rounded-full"
-                  onClick={() => setFormState('modifyTeam')}
-                />
-                <span className="text-gray-500 group-has-checked:text-white dark:text-gray-400">Modify Team</span>
-              </label>
-              {/* Remove Team */}
-              <label className="group relative rounded-full px-2.5 py-1 has-checked:bg-indigo-600 dark:has-checked:bg-indigo-500">
-                <input
-                  defaultValue="removeTeam"
-                  name="switcher"
-                  type="radio"
-                  className="absolute inset-0 appearance-none rounded-full"
-                  onClick={() => setFormState('removeTeam')}
-                />
-                <span className="text-gray-500 group-has-checked:text-white dark:text-gray-400">Remove Team</span>
-              </label>
-              {/* Add Division */}
-              <label className="group relative rounded-full px-2.5 py-1 has-checked:bg-indigo-600 dark:has-checked:bg-indigo-500">
-                <input
-                  defaultValue="addDivision"
-                  name="switcher"
-                  type="radio"
-                  className="absolute inset-0 appearance-none rounded-full"
-                  onClick={() => setFormState('addDivision')}
-                />
-                <span className="text-gray-500 group-has-checked:text-white dark:text-gray-400">Add Division</span>
-              </label>
-              {/* Remove Division */}
-              <label className="group relative rounded-full px-2.5 py-1 has-checked:bg-indigo-600 dark:has-checked:bg-indigo-500">
-                <input
-                  defaultValue="removeDivision"
-                  name="switcher"
-                  type="radio"
-                  className="absolute inset-0 appearance-none rounded-full"
-                  onClick={() => setFormState('removeDivision')}
-                />
-                <span className="text-gray-500 group-has-checked:text-white dark:text-gray-400">Remove Division</span>
-              </label>
-              {/* Game Scheduler */}
-              <label className="group relative rounded-full px-2.5 py-1 has-checked:bg-indigo-600 dark:has-checked:bg-indigo-500">
-                <input
-                  defaultValue="gameScheduler"
-                  name="switcher"
-                  type="radio"
-                  className="absolute inset-0 appearance-none rounded-full"
-                  onClick={() => setFormState('gameScheduler')}
-                />
-                <span className="text-gray-500 group-has-checked:text-white dark:text-gray-400">Set Table</span>
-              </label>
-              {/* Game Remover */}
-              <label className="group relative rounded-full px-2.5 py-1 has-checked:bg-indigo-600 dark:has-checked:bg-indigo-500">
-                <input
-                  defaultValue="gameRemover"
-                  name="switcher"
-                  type="radio"
-                  className="absolute inset-0 appearance-none rounded-full"
-                  onClick={() => setFormState('gameRemover')}
-                />
-                <span className="text-gray-500 group-has-checked:text-white dark:text-gray-400">Remove Games</span>
-              </label>
-              {/* Game Archiver */}
-              <label className="group relative rounded-full px-2.5 py-1 has-checked:bg-indigo-600 dark:has-checked:bg-indigo-500">
-                <input
-                  defaultValue="archiveGames"
-                  name="switcher"
-                  type="radio"
-                  className="absolute inset-0 appearance-none rounded-full"
-                  onClick={() => setFormState('archiveGames')}
-                />
-                <span className="text-gray-500 group-has-checked:text-white dark:text-gray-400">Archive Games</span>
-              </label>
-              {/* Season Reset */}
-              <label className="group relative rounded-full px-2.5 py-1 has-checked:bg-indigo-600 dark:has-checked:bg-indigo-500">
-                <input
-                  defaultValue="seasonReset"
-                  name="switcher"
-                  type="radio"
-                  className="absolute inset-0 appearance-none rounded-full"
-                  onClick={() => setFormState('seasonReset')}
-                />
-                <span className="text-gray-500 group-has-checked:text-white dark:text-gray-400">Reset Season</span>
-              </label>
+        {tasks.map((task, taskIndex) => (
+          <li key={taskIndex} className='flow-root'>
+            <div className="relative -m-2 flex items-center space-x-4 rounded-xl p-2 focus-within:outline-2 focus-within:outline-indigo-600 hover:bg-gray-50 dark:focus-within:outline-indigo-500 dark:hover:bg-white/5">
+              <div className={classNames(task.background, 'flex size-16 shrink-0 items-center justify-center rounded-lg')}>
+                <task.icon aria-hidden='true' className='size-6 text-white'/>
+              </div>
+              <div onClick={handleButtonClick}>
+                <h3 className="text-sm font-medium text-gray-900 dark:text-white">
+                  <p className="focus:outline-hidden">
+                    <span aria-hidden='true' className='absolute inset-0' id={task.value}></span>
+                    <span>{task.title}</span>
+                    <span aria-hidden='true'> &rarr;</span>
+                  </p>
+                </h3>             
+              </div>
             </div>
-          </fieldset>
+          </li>
+        ))}
+      </ul>
+
+      <Dialog open={open} onClose={setOpen} className="relative z-10">
+        <DialogBackdrop
+          transition
+          className="fixed inset-0 bg-gray-500/75 transition-opacity data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in dark:bg-gray-900/50"
+        />
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <DialogPanel
+              transition
+              className="relative transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left shadow-xl transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:w-full sm:max-w-lg sm:p-6 data-closed:sm:translate-y-0 data-closed:sm:scale-95 dark:bg-gray-800 dark:outline dark:-outline-offset-1 dark:outline-white/10"
+            >
+              <div className="imx-auto max-w-7xl px-6 lg:px-8">
+                {currentForm === 'addTeam' && (<AddTeamForm />)}
+                {currentForm === 'removeTeam' && (<RemoveTeamForm />)}
+                {currentForm === 'addDivision' && (<AddDivisionForm />)}
+                {currentForm === 'removeDivision' && (<RemoveDivisionForm />)}
+                {currentForm === 'gameScheduler' && (<GameScheduler />)}
+                {currentForm === 'removeGame' && (<RemoveGamesForm />)}
+                {currentForm === 'archiveGames' && (<ArchiveGamesForm />)}
+                {currentForm === 'seasonReset' && (<ResetSeasonForm />)}
+              </div>  
+            </DialogPanel>
+          </div>
         </div>
-        <div className="imx-auto max-w-7xl px-6 lg:px-8">
-          {currentForm === 'addTeam' && (<AddTeamForm />)}
-          {currentForm === 'removeTeam' && (<RemoveTeamForm />)}
-          {currentForm === 'addDivision' && (<AddDivisionForm />)}
-          {currentForm === 'removeDivision' && (<RemoveDivisionForm />)}
-          {currentForm === 'gameScheduler' && (<GameScheduler />)}
-          {currentForm === 'gameRemover' && (<RemoveGamesForm />)}
-          {currentForm === 'archiveGames' && (<ArchiveGamesForm />)}
-          {currentForm === 'seasonReset' && (<ResetSeasonForm />)}
-        </div>
-      </div>
+      </Dialog>
     </div>
   )
 }
