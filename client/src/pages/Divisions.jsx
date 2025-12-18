be1629710188f639f908d205812d9d1324114192
TimWong <timwong.tw93@gmail.com>
2025-12-18 22:48:20 +1100

    fixed typo in divisions page

--- client/src/pages/Divisions.jsx
+++ client/src/pages/Divisions.jsx
@@ -41,7 +41,7 @@ const Divisions = () => {
         <div>
           <h2>Sunday</h2>
           <h2>Divisions</h2>
-          
+          <div>
             {sundayDivs.map((div) => (
               <a key={div._id} 
                     className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700
@@ -54,6 +54,7 @@ const Divisions = () => {
                 </div>
               </a>
             ))}
+          </div>
         </div>
         {/* Monday Divs */}
         <div>
