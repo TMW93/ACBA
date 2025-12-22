5ff65cd72ac3348d391245a6fb1eda28e6e4268e
TimWong <timwong.tw93@gmail.com>
2025-12-22 23:50:55 +1100

    updated nav

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -1,6 +1,9 @@
 import {useState} from 'react'
-import { Disclosure, DisclosureButton, DisclosurePanel} from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import { useQuery } from '@apollo/client/react'
+import { QUERY_ME } from '../../utils/queries'
+import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import Auth from '../../utils/auth'
 import ACBALogo from '../../assets/icons/acbaLogo.png'
 
 const navigation = [
@@ -30,6 +33,11 @@ const navigation = [
     current: false,
   },
   {
+    name: 'Login', 
+    href: '/login',
+    current: false,
+  },
+  {
     name: 'Admin', 
     href: '/admin',
     current: false,
@@ -41,61 +49,140 @@ function classNames(...classes) {
 };
 
 export default function Nav() {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  // const {loading, error, data} = useQuery(QUERY_ME);
+  // if(loading) {
+  //   return null;
+  // }
+  // if(error) {
+  //   return `Error! ${error}`;
+  // }
+
+  // const user = data.user || [];
+
+  // console.log(user);
+
+  const adminNav = () => {
+    if(Auth.loggedIn()) {
+      return (
+        <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+          {/* Profile dropdown */}
+          <Menu as="div" className="relative ml-3">
+            <MenuButton className="relative flex rounded-full focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
+              <span className="absolute -inset-1.5" />
+              <span className="sr-only">Open user menu</span>
+              <img
+                alt=""
+                src={ACBALogo}
+                className="size-8 rounded-full bg-gray-800 outline -outline-offset-1 outline-white/10"
+              />
+            </MenuButton>
+            <MenuItems
+              transition
+              className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg outline outline-black/5 transition data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in dark:bg-gray-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10"
+            >
+              <MenuItem>
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden dark:text-gray-300 dark:data-focus:bg-white/5"
+                >
+                  Your profile
+                </a>
+              </MenuItem>
+              <MenuItem>
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden dark:text-gray-300 dark:data-focus:bg-white/5"
+                >
+                  Admin
+                </a>
+              </MenuItem>
+              <MenuItem>
+                <a
+                  href="/"
+                  className="block px-4 py-2 text-sm text-gray-700 data-focus:bg-gray-100 data-focus:outline-hidden dark:text-gray-300 dark:data-focus:bg-white/5"
+                  onClick={() => Auth.logout()}
+                >
+                  Sign out
+                </a>
+              </MenuItem>
+            </MenuItems>
+          </Menu>
+        </div>
+      );
+    } else {
+      <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+        <button
+          type="button"
+          className="relative rounded-full p-1 text-gray-400 focus:outline-2 focus:outline-offset-2 focus:outline-indigo-500 dark:hover:text-white"
+        >
+          <span className="absolute -inset-1.5" />
+          <span className="sr-only">Sign In</span>
+          <BellIcon aria-hidden="true" className="size-6" />
+        </button>   
+      </div>
+    }
+  };
 
   return (
     <Disclosure
       as='nav'
       className="absolute inset-x-0 top-0 z-50 bg-gray-800 dark:bg-gray-800/50 dark:after:pointer-events-none dark:after:absolute dark:after:inset-x-0 dark:after:bottom-0 dark:after:h-px dark:after:bg-white/10"
     >
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-        <div className="flex h-16 justify-between">
-          <div className='flex'>
-            <div className="mr-2 -ml-2 flex items-center md:hidden">
-              {/* Mobile Menu Button */}
-              <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-white/5 hover:text-white focus:outline-2 focus:-outline-offset-1 focus:outline-indigo-500">
-                <span className="absolute -inset-0.5" />
-                <span className="sr-only">Open main menu</span>
-                <Bars3Icon aria-hidden="true" className="block size-6 group-data-open:hidden" />
-                <XMarkIcon aria-hidden="true" className="hidden size-6 group-data-open:block" />
-              </DisclosureButton>
-            </div>
-            {/* Navbar Icon */}
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="relative flex h-16 items-center justify-between">
+          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+            {/* Mobile Menu Button */}
+            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-white/5 hover:text-white focus:outline-2 focus:-outline-offset-1 focus:outline-indigo-500">
+              <span className="absolute -inset-0.5" />
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon aria-hidden="true" className="block size-6 group-data-open:hidden" />
+              <XMarkIcon aria-hidden="true" className="hidden size-6 group-data-open:block" />
+            </DisclosureButton>
+          </div>
+          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+            {/* Logo */}
             <div className="flex shrink-0 items-center">
               <img
-                alt='ACBA Logo'
+                alt="ACBA"
                 src={ACBALogo}
-                className='h-8 w-auto'
+                className="h-8 w-auto"
               />
             </div>
             {/* Navbar Items */}
-            <div className="hidden md:ml-6 md:flex md:items-center md:space-x-4">
-              {navigation.map((item) => (
-                <a
-                  key={item.name}
-                  href={item.href}
-                  aria-current={item.current ? 'page' : undefined}
-                  className={classNames(item.current ? 'bg-gray-900 text-white dark:bg-gray-950/50' : 'text-gray-300 hover:bg-white/5 hover:text-white',
-                    'rounded-md px-3 py-2 text-sm font-medium',
-                  )}
-                >
-                  {item.name}
-                </a>
-              ))}
+            <div className="hidden sm:ml-6 sm:block">
+              <div className="flex space-x-4">
+                {navigation.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    aria-current={item.current ? 'page' : undefined}
+                    className={classNames(item.current ? 'bg-gray-900 text-white dark:bg-gray-950/50' : 'text-gray-300 hover:bg-white/5 hover:text-white',
+                      'rounded-md px-3 py-2 text-sm font-medium',
+                    )}
+                  >
+                    {item.name}
+                  </a>
+                ))}
+              </div>
             </div>
           </div>
+          {adminNav()}
         </div>
       </div>
 
-      <DisclosurePanel>
-        <div className="space-y-1 px-2 pt-2 pb-3 sm:px-3">
+      <DisclosurePanel className="sm:hidden">
+        <div className="space-y-1 px-2 pt-2 pb-3">
           {navigation.map((item) => (
             <DisclosureButton
               key={item.name}
-              as='a'
+              as="a"
               href={item.href}
               aria-current={item.current ? 'page' : undefined}
-              className={classNames(item.current ? 'bg-gray-900 text-white dark:bg-gray-950/50' : 'text-gray-300 hover:bg-white/5 hover:text-white',
+              className={classNames(
+                item.current
+                  ? 'bg-gray-900 text-white dark:bg-gray-950/50'
+                  : 'text-gray-300 hover:bg-white/5 hover:text-white',
                 'block rounded-md px-3 py-2 text-base font-medium',
               )}
             >
