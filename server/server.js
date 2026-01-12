25c9c75462c8c050c33be51d7ce1a785826b0db1
TimWong <timwong.tw93@gmail.com>
2026-01-12 19:21:43 +1100

    updated code

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ const startApolloServer = async () => {
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
 
-    app.get('*', (req, res) => {
+    app.get('/:path(*)', (req, res) => {
       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
     });
   }
