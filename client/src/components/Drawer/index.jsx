f7aa6403943dc5bf41e5c64805bff096a6d30784
TimWong <timwong.tw93@gmail.com>
2026-02-20 19:43:56 +1100

    updated styling

--- client/src/components/Drawer/index.jsx
+++ client/src/components/Drawer/index.jsx
@@ -33,12 +33,12 @@ export default function Drawer({ open, setOpen, children }) {
                 leaveFrom="translate-x-0"
                 leaveTo="translate-x-full"
               >
-                <DialogPanel className="pointer-events-auto w-screen w-screen max-w-2xl">
+                <DialogPanel className="pointer-events-auto w-screen max-w-2xl">
                   <div className="relative flex w-full h-full flex-col overflow-y-auto bg-white py-6 shadow-xl dark:bg-gray-800">
                     <div className="px-4 sm:px-6 flex items-start justify-between">
-                      <DialogTitle className="mt-15 text-base font-semibold text-gray-900 dark:text-white">
-                        <div className='w-full'>
-                          <h2 className='mb-5 text-base/7 text-gray-800'>Our Facebook</h2>
+                      <DialogTitle className="text-base font-semibold text-gray-900 dark:text-white">
+                        <div>
+                          <h2 className='text-base/7 text-gray-800'>Our Facebook</h2>
                         </div>
                       </DialogTitle>
                       <button
@@ -50,8 +50,8 @@ export default function Drawer({ open, setOpen, children }) {
                         <XMarkIcon className="h-6 w-6" aria-hidden="true" />
                       </button>
                     </div>
-                    <div className="px-4 sm:px-6 mt-4 w-full"> <FacebookProfileEmbed /> </div>
-                    <div className="relative mt-6 flex-1 px-4 sm:px-6">{children}</div>
+                    <div className="px-4 sm:px-6 mt-4 w-full items-center"> <FacebookProfileEmbed /> </div>
+                    {/* <div className="relative mt-6 flex-1 px-4 sm:px-6">{children}</div> */}
                   </div>
                 </DialogPanel>
               </TransitionChild>
--- client/src/components/FacebookPlugin/index.jsx
+++ client/src/components/FacebookPlugin/index.jsx
@@ -2,7 +2,7 @@ const FacebookProfileEmbed = () => {
   return (
     <div className="w-full overflow-hidden">
       <div className="relative w-full">
-        <div className="origin-top-left scale-100 sm:scale-100 xs:scale-[0.75]">
+        <div className="origin-top-left scale-100 sm:scale-100 xs:scale-[0.5]">
           <iframe
             src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FAustralianChineseBasketballAssociation&tabs=timeline&width=500&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true"
             className="border-0"
