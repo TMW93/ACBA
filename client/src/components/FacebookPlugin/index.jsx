1cb345a8ae7673c53ae20e200fe4e615a76bc3c4
TimWong <timwong.tw93@gmail.com>
2026-02-20 18:28:18 +1100

    edited styling

--- client/src/components/FacebookPlugin/index.jsx
+++ client/src/components/FacebookPlugin/index.jsx
@@ -1,13 +1,23 @@
 const FacebookProfileEmbed = () => {
   return (
-    <div className="w-full">
-      <iframe
-        src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FAustralianChineseBasketballAssociation&tabs=timeline&width=500&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true"
-        className="w-full border-0"
-        style={{ height: "500px" }}
-        data-adapt-container-width="true"
-        allowFullScreen={false}
-      />
+    // <div className="w-full">
+    //   <iframe
+    //     src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FAustralianChineseBasketballAssociation&tabs=timeline&width=500&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true"
+    //     className="w-full border-0"
+    //     style={{ height: "500px" }}
+    //     data-adapt-container-width="true"
+    //     allowFullScreen={false}
+    //   />
+    // </div>
+    <div className="w-full overflow-hidden">
+      <div className="relative w-full" style={{ paddingTop: "150%" }}>
+        <iframe
+          src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FAustralianChineseBasketballAssociation&tabs=timeline&width=500&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true"
+          className="absolute top-0 left-0 w-full h-full border-0"
+          data-adapt-container-width="true"
+          allowFullScreen={false}
+        />
+      </div>
     </div>
   );
 };
